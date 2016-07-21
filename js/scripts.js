07d35f4c7c3b7030321e0c0d7ae2871fe02f3bc1
Alexandr Gerasimov <888882008@mail.ru>
2016-07-21 11:44:31 +0300

    Правки, правки, правки 👌

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-      var link = document.querySelector(".btn-contact-us");
-      var searchForm = document.querySelector(".popup-form");
+var link = document.querySelector(".btn-contact-us");
+
+      var popup = document.querySelector(".popup-form");
+      var close = popup.querySelector(".btn-popup-close");
+
+      var login = popup.querySelector("[name=name]");
 
       link.addEventListener("click", function(event) {
-          event.preventDefault();
-          if (searchForm.classList.contains("map-form-show")) {
-              searchForm.classList.add("map-form-close");
-              searchForm.classList.remove("map-form-show");
-          } else {
-              searchForm.classList.add("map-form-show");
-              searchForm.classList.remove("map-form-close");
-          };
+        event.preventDefault();
+        popup.classList.remove("popup-form-close");
+        popup.classList.add("popup-form-show");
+        login.focus();
+      });
+
+      close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("popup-form-show");
+        popup.classList.add("popup-form-close");
       });
